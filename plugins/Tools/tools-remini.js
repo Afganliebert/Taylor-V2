2d18154787d24d9a83247b5332f51331ba3dfcbb
Afganliebert <162288764+Afganliebert@users.noreply.github.com>
2024-05-06 13:29:40 +0700

    Update tools-remini.js

--- plugins/Tools/tools-remini.js
+++ plugins/Tools/tools-remini.js
@@ -1,33 +1,89 @@
-import fetch from "node-fetch";
-import uploadImage from "../../lib/uploadImage.js";
-import {
-    remini
-} from "../../lib/scraper/scraper-tool.js";
+/*
+wa.me/6282285357346
+github: https://github.com/sadxzyq
+Instagram: https://instagram.com/tulisan.ku.id
+ini wm gw cok jan di hapus
+*/
 
-let handler = async (m, {
-    conn
-}) => {
-    try {
-        const q = m.quoted || m;
-        const mime = (q.msg || q).mimetype || "";
+import FormData from "form-data"
+import Jimp from "jimp"
 
-        if (/image/g.test(mime) && !/webp/g.test(mime)) {
-            const img = await q.download?.();
-            const out = await uploadImage(img);
-            const response = await remini(out, 'm3yl4zGsURJtiODuVl4OnGhrwfgMwtTnTlaLmYJHW34UhB02');
+async function processing(urlPath, method) {
+  return new Promise(async (resolve, reject) => {
+    let Methods = ["enhance", "recolor", "dehaze"];
+    Methods.includes(method) ? (method = method) : (method = Methods[0]);
+    let buffer,
+      Form = new FormData(),
+      scheme = "https" + "://" + "inferenceengine" + ".vyro" + ".ai/" + method;
+    Form.append("model_version", 1, {
+      "Content-Transfer-Encoding": "binary",
+      contentType: "multipart/form-data; charset=uttf-8",
+    });
+    Form.append("image", Buffer.from(urlPath), {
+      filename: "enhance_image_body.jpg",
+      contentType: "image/jpeg",
+    });
+    Form.submit(
+      {
+        url: scheme,
+        host: "inferenceengine" + ".vyro" + ".ai",
+        path: "/" + method,
+        protocol: "https:",
+        headers: {
+          "User-Agent": "okhttp/4.9.3",
+          Connection: "Keep-Alive",
+          "Accept-Encoding": "gzip",
+        },
+      },
+      function (err, res) {
+        if (err) reject();
+        let data = [];
+        res
+          .on("data", function (chunk, resp) {
+            data.push(chunk);
+          })
+          .on("end", () => {
+            resolve(Buffer.concat(data));
+          });
+        res.on("error", (e) => {
+          reject();
+        });
+      }
+    );
+  });
+}
 
-            if (response) {
-                await conn.sendFile(m.chat, response, "", "*[ REMINI ]*", m);
-            }
-        } else {
-            throw new Error("Reply imagenya");
+let handler = async (m, { conn, usedPrefix, command }) => {
+      conn.enhancer = conn.enhancer ? conn.enhancer : {};
+      if (m.sender in conn.enhancer)
+        throw "sabar masih ada sesi yang tadi jadi tunggu selesai ya🙄";
+      let q = m.quoted ? m.quoted : m;
+      let mime = (q.msg || q).mimetype || q.mediaType || "";
+      if (!mime) throw `Fotonya Mana Kak?`;
+      if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`;
+      else conn.enhancer[m.sender] = true;
+          const react = {react: {text: "⏳", key: m.key}}
+
+      let img = await q.download?.();
+      let error;
+      try {
+        const This = await processing(img, "enhance");
+        conn.sendFile(m.chat, This, "", "*Result from*:" + command, m).then(_ => {
+            const react = {react: {text: "✅", key: m.key}}
+            
+        })
+      } catch (er) {
+        error = true;
+      } finally {
+        if (error) {
+              const react = {react: {text: "❗", key: m.key}}
+
         }
-    } catch (e) {
-        await m.reply(eror);
-    }
+        delete conn.enhancer[m.sender];
+      }
 };
-
 handler.help = ["remini"];
-handler.tags = ["tools"];
+handler.tags = ["ai"];
+handler.limit = true;
 handler.command = ["remini"];
 export default handler;
